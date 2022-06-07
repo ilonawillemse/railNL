12c578e6161577b88cc06cf9f2ee0bbca61af21e
ilonawillemse <ilonacw99@live.nl>
2022-06-07 12:02:01 +0200

    getting started

--- Code.py
+++ Code.py
@@ -1,1 +1,36 @@
-# Code
+"""
+=================================================
+Code.py
+
+Ilona Willemse, Wesley Korff, Anouk Van Valkengoed
+
+No way, Railway
+
+Object based railway traject
+=================================================
+"""
+
+class Station():
+    "Station Object"
+    def __init__(self):
+        self.name = ''
+        self.connections  = []
+        self.visited = 0
+        self.xcor = 0
+        self.ycor = 0
+        
+
+class Model():
+    "Railway Model"
+    def __init__(self):
+        self.stations = []
+        self.traject = {}
+        self.score = 0
+    
+    def fraction_visited(self):
+        pass
+    
+    def quality_score(self):
+        pass
+        # return p * 10000 - (T * 100 + Min)
+
