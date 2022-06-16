2623b1b266bd6344b7b49909a5ddeb43b6f4defb
ilonawillemse <ilonacw99@live.nl>
2022-06-16 12:31:17 +0200

    improved hillclimber interaction

--- Code.py
+++ Code.py
@@ -17,7 +17,7 @@ from helpers import output_generate, quality_score
 from visualize import visualization
 from loader import load_stations, add_connections
 from algorithms.baseline import starting_trajects
-from algorithms.hillclimber import starting_trajects_hillclimber, run_hillclimber
+from algorithms.hillclimber import run_hillclimber
 from algorithms.greedy import get_started
 
 class Model():
@@ -31,7 +31,6 @@ class Model():
         self.traject = []
         self.time_dict = {}
         self.all_connections = add_connections(self.stations)
-        #hillclimber addition
         self.visited_connections = []     
 
     def baseline(self):
@@ -43,46 +42,16 @@ class Model():
         quality_score(self) 
 
 
-    def hillclimber_base(self):
-        starting_trajects_hillclimber(self)
-        quality_score(self)
-
 if __name__ == "__main__":
-# ---------------greedy-----------
-    # model = Model()
-    # model.baseline()
-    # best_traject = []
-    # all_scores = []
-    # highest_score = 0
-    # with open('output/histo_data.csv', 'w') as output_file:
-    #     for i in range(10000):
-    #         model = Model()
-    #         model.greedy()
-    #         writer = csv.writer(output_file) 
-    #         if model.score > highest_score:
-    #             best_traject = model.traject
-    #             highest_score = model.score
-    #             best_fraction = model.fraction
-    #         score = model.score
-    #         all_scores.append(score)
-    #         writer.writerow([score])
-    #         print(i)
-    # output_generate(best_traject, highest_score, best_fraction)
-    # data = all_scores
-    # num_bins = 100 # <- number of bins for the histogram
-    # plt.hist(data, num_bins)
-    # plt.savefig("output/histogramgreedy.png")
-    # visualization(model, best_traject)
-
 
 # # ---------------------------------baseline-------------------------------
 #     best_traject = []
 #     all_scores = []
 #     highest_score = 0
 #     with open('output/histo_data.csv', 'w') as output_file:
-#         for i in range(10):
+#         for i in range(100):
 #             model = Model()
-#             model.run()
+#             model.baseline()
 #             writer = csv.writer(output_file) 
 #             if model.score > highest_score:
 #                 best_traject = model.traject
@@ -99,11 +68,46 @@ if __name__ == "__main__":
 #     plt.savefig("output/histogramtest.png")
 #     visualization(model, best_traject)
 
-## -------------------------------hillclimber-------------------------------
-    model = Model()
-    model.hillclimber_base()
 
-    best_traject, best_score, best_fraction = run_hillclimber(model)
+# # ---------------------------------greedy-------------------------------
+#     best_traject = []
+#     all_scores = []
+#     highest_score = 0
+#     with open('output/histo_data.csv', 'w') as output_file:
+#         for i in range(100):
+#             model = Model()
+#             model.greedy()
+#             writer = csv.writer(output_file) 
+#             if model.score > highest_score:
+#                 best_traject = model.traject
+#                 highest_score = model.score
+#                 best_fraction = model.fraction
+#             score = model.score
+#             all_scores.append(score)
+#             writer.writerow([score])
+#             print(i)
+#     output_generate(best_traject, highest_score, best_fraction)
+#     data = all_scores
+#     num_bins = 100 # <- number of bins for the histogram
+#     plt.hist(data, num_bins)
+#     plt.savefig("output/histogramgreedy.png")
+#     visualization(model, best_traject)
 
-    output_generate(best_traject, best_score, best_fraction)
-  
+
+## -------------------------------hillclimber baseline------mind: change import in hillclimber.p-------------------------
+    # model = Model()
+    # model.baseline()
+
+    # best_traject, best_score, best_fraction = run_hillclimber(model)
+
+    # output_generate(best_traject, best_score, best_fraction)
+
+
+## -------------------------------hillclimber greedy-----mind: change import in hillclimber.py--------------------------
+    # model = Model()
+    # model.greedy()
+
+    # best_traject, best_score, best_fraction = run_hillclimber(model)
+
+    # output_generate(best_traject, best_score, best_fraction)
+    # visualization(model, best_traject)
